0bb1beb8dfc2d5c39db06868d270277f698ee004
Hejin0-0 <lovenstein0624@gmail.com>
2024-08-15 13:37:25 +0900

    #02 - Landing Screen

--- eraser.io/app/page.tsx
+++ eraser.io/app/page.tsx
@@ -1,6 +1,15 @@
+// https://www.youtube.com/watch?v=sxn2GPQjJgc     17:36
+
 import { Button } from "@/components/ui/button";
 import Image from "next/image";
+import Header from "./_components/Header";
+import Hero from "./_components/Hero";
 
 export default function Home() {
-	return <div></div>;
+	return (
+		<div>
+			<Header />
+			<Hero />
+		</div>
+	);
 }
