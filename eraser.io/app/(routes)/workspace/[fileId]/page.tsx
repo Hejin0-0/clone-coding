915a714618204aafadf17cad24cb92eb5f4fe116
Hejin0-0 <lovenstein0624@gmail.com>
2024-08-22 16:58:08 +0900

    #15 - Add Editor.jS Document

--- eraser.io/app/(routes)/workspace/[fileId]/page.tsx
+++ eraser.io/app/(routes)/workspace/[fileId]/page.tsx
@@ -1,5 +1,6 @@
 import React from "react";
 import WorkspaceHeader from "./_components/WorkspaceHeader";
+import Editor from "./_components/Editor";
 
 function Workspace() {
 	return (
@@ -10,7 +11,9 @@ function Workspace() {
 			<div className="grid grid-cols-1 md:grid-cols-2">
 				<div>
 					{/* Document */}
-					<div className="bg-blue-400 h-screen">Document</div>
+					<div className="h-screen">
+						<Editor />
+					</div>
 				</div>
 
 				{/* Whiteboard / Canvas */}
