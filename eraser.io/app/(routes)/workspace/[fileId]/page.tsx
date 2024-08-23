40b14ec364f114f9c5d24c9376a2c6aa869299a4
Hejin0-0 <lovenstein0624@gmail.com>
2024-08-23 15:13:41 +0900

    #16 - Save Document

--- eraser.io/app/(routes)/workspace/[fileId]/page.tsx
+++ eraser.io/app/(routes)/workspace/[fileId]/page.tsx
@@ -1,18 +1,27 @@
-import React from "react";
+"use client";
+
+import React, { useEffect, useState } from "react";
 import WorkspaceHeader from "./_components/WorkspaceHeader";
 import Editor from "./_components/Editor";
 
-function Workspace() {
+function Workspace({ params }: any) {
+	const [triggerSave, setTriggerSave] = useState(false);
+	useEffect(() => {
+		console.log("FILEID", params.fileId);
+	}, []);
 	return (
 		<div>
-			<WorkspaceHeader />
+			<WorkspaceHeader onSave={() => setTriggerSave(!triggerSave)} />
 
 			{/* Workspace Layout */}
 			<div className="grid grid-cols-1 md:grid-cols-2">
 				<div>
 					{/* Document */}
 					<div className="h-screen">
-						<Editor />
+						<Editor
+							onSaveTrigger={triggerSave}
+							fileId={params.fileId}
+						/>
 					</div>
 				</div>
 
