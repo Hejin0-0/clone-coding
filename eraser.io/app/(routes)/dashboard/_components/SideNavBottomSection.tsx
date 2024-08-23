edaf829f8692d8ba7fd76c9444151af1589f7f7f
Hejin0-0 <lovenstein0624@gmail.com>
2024-08-23 16:48:45 +0900

    #21 - Pricing Sect

--- eraser.io/app/(routes)/dashboard/_components/SideNavBottomSection.tsx
+++ eraser.io/app/(routes)/dashboard/_components/SideNavBottomSection.tsx
@@ -12,6 +12,8 @@ import {
 	DialogTrigger,
 } from "@/components/ui/dialog";
 import { Input } from "@/components/ui/input";
+import Constant from "@/app/_constant/Constant";
+import PricingDialog from "./PricingDialog";
 
 function SideNavBottomSection({ onFileCreate, totalFiles }: any) {
 	const menuList = [
@@ -34,9 +36,7 @@ function SideNavBottomSection({ onFileCreate, totalFiles }: any) {
 			path: "",
 		},
 	];
-
 	const [fileInput, setFileInput] = useState("");
-
 	return (
 		<div>
 			{menuList.map((menu, index) => (
@@ -56,31 +56,38 @@ function SideNavBottomSection({ onFileCreate, totalFiles }: any) {
 						New File
 					</Button>
 				</DialogTrigger>
-
-				<DialogContent>
-					<DialogHeader>
-						<DialogTitle>Create New File</DialogTitle>
-						<DialogDescription>
-							<Input
-								placeholder="Enter File Name"
-								className="mt-3"
-								onChange={(e) => setFileInput(e.target.value)}
-							/>
-						</DialogDescription>
-					</DialogHeader>
-					<DialogFooter className="">
-						<DialogClose asChild>
-							<Button
-								type="button"
-								className="bg-blue-600 hover:bg-blue-700"
-								disabled={!(fileInput && fileInput.length > 3)}
-								onClick={() => onFileCreate(fileInput)}
-							>
-								Create
-							</Button>
-						</DialogClose>
-					</DialogFooter>
-				</DialogContent>
+				{totalFiles < Constant.MAX_FREE_FILE ? (
+					<DialogContent>
+						<DialogHeader>
+							<DialogTitle>Create New File</DialogTitle>
+							<DialogDescription>
+								<Input
+									placeholder="Enter File Name"
+									className="mt-3"
+									onChange={(e) =>
+										setFileInput(e.target.value)
+									}
+								/>
+							</DialogDescription>
+						</DialogHeader>
+						<DialogFooter className="">
+							<DialogClose asChild>
+								<Button
+									type="button"
+									className="bg-blue-600 hover:bg-blue-700"
+									disabled={
+										!(fileInput && fileInput.length > 3)
+									}
+									onClick={() => onFileCreate(fileInput)}
+								>
+									Create
+								</Button>
+							</DialogClose>
+						</DialogFooter>
+					</DialogContent>
+				) : (
+					<PricingDialog />
+				)}
 			</Dialog>
 
 			{/* Progress Bar  */}
@@ -90,11 +97,13 @@ function SideNavBottomSection({ onFileCreate, totalFiles }: any) {
 					style={{ width: `${(totalFiles / 5) * 100}%` }}
 				></div>
 			</div>
+
 			<h2 className="text-[12px] mt-3">
-				<strong>{totalFiles}</strong> out <strong>5</strong> files used
+				<strong>{totalFiles}</strong> out of{" "}
+				<strong>{Constant.MAX_FREE_FILE}</strong> files used
 			</h2>
 			<h2 className="text-[12px] mt-1">
-				Upgrade your plan unlimited access.
+				Upgrade your plan for unlimited access.
 			</h2>
 		</div>
 	);
