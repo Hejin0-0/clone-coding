ce5c1d98f1f64572e9a69efdca612b9dd1a6b503
Hejin0-0 <lovenstein0624@gmail.com>
2024-08-29 15:01:08 +0900

    #07 - Empty States

--- miro/app/(dashboard)/page.tsx
+++ miro/app/(dashboard)/page.tsx
@@ -1,7 +1,27 @@
-import React from "react";
+"use client";
 
-function DashboardPage() {
-	return <div>Dashboard Root page</div>;
+import { useOrganization } from "@clerk/nextjs";
+import { EmptyOrg } from "./_components/EmptyOrg";
+import { BoardList } from "./_components/BoardList";
+
+interface DashboardPageProps {
+	searchParams: {
+		search?: string;
+		favorites?: string;
+	};
 }
 
-export default DashboardPage;
+export default function DashboardPage({ searchParams }: DashboardPageProps) {
+	const { organization } = useOrganization();
+
+	return (
+		<div className="flex-1 h-[calc(100%-80px)] p-6">
+			{/* {JSON.stringify(searchParams)} */}
+			{!organization ? (
+				<EmptyOrg />
+			) : (
+				<BoardList orgId={organization.id} query={searchParams} />
+			)}
+		</div>
+	);
+}
