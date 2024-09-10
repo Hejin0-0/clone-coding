e2c0921d9a5bcdd7e6a7840c57b6f43f18d78c57
Hejin0-0 <lovenstein0624@gmail.com>
2024-09-10 17:45:04 +0900

    #21 - Insert Layout (err -> @clerk 'middleware.ts')

--- miro/app/api/liveblocks-auth/route.ts
+++ miro/app/api/liveblocks-auth/route.ts
@@ -1,7 +1,7 @@
+import { auth, currentUser } from "@clerk/nextjs";
 import { Liveblocks } from "@liveblocks/node";
 import { api } from "@/convex/_generated/api";
 import { ConvexHttpClient } from "convex/browser";
-import { auth, currentUser } from "@clerk/nextjs/server";
 
 const convex = new ConvexHttpClient(process.env.NEXT_PUBLIC_CONVEX_URL!);
 
--- miro/middleware.ts
+++ miro/middleware.ts
@@ -1,24 +1,12 @@
-import { clerkMiddleware, createRouteMatcher } from "@clerk/nextjs/server";
+import { withClerkMiddleware } from "@clerk/nextjs/server";
 
-const isProtectedRoute = createRouteMatcher(["/(.*)", "/"]);
-
-export default clerkMiddleware((auth, req) => {
-	if (isProtectedRoute(req)) auth().protect();
+export default withClerkMiddleware({
+	publicRoutes: ["/contact", "/api(.)*"],
 });
 
 export const config = {
-	matcher: ["/((?!.*\\..*|_next).*)", "/", "/(api|trpc)(.*)"],
+	matcher: [
+		"/((?!_next|[^?]*\\.(?:html?|css|js(?!on)|jpe?g|webp|png|gif|svg|ttf|woff2?|ico|csv|docx?|xlsx?|zip|webmanifest)).*)",
+		"/(api|trpc)(.*)",
+	],
 };
-
-// import { clerkMiddleware } from "@clerk/nextjs/server";
-
-// export default clerkMiddleware();
-
-// export const config = {
-// 	matcher: [
-// 		// Skip Next.js internals and all static files, unless found in search params
-// 		"/((?!_next|[^?]*\\.(?:html?|css|js(?!on)|jpe?g|webp|png|gif|svg|ttf|woff2?|ico|csv|docx?|xlsx?|zip|webmanifest)).*)",
-// 		// Always run for API routes
-// 		"/(api|trpc)(.*)",
-// 	],
-// };
