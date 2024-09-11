424c9914ad97a6d6d18c89f80cfff51afafc5a58
Hejin0-0 <lovenstein0624@gmail.com>
2024-09-11 15:52:32 +0900

    #22 - Select Layer (err -> @clerk)

--- miro/app/board/[boardId]/_components/Canvas.tsx
+++ miro/app/board/[boardId]/_components/Canvas.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import { useCallback, useState } from "react";
+import { useCallback, useMemo, useState } from "react";
 import {
 	Camera,
 	CanvasMode,
@@ -18,8 +18,9 @@ import {
 	useCanRedo,
 	useMutation,
 	useStorage,
+	useOthersMapped,
 } from "@/liveblocks.config";
-import { pointerEventToCanvasPoint } from "@/lib/utils";
+import { connectionIdToColor, pointerEventToCanvasPoint } from "@/lib/utils";
 import { CursorsPresence } from "./CursorsPresence";
 import { nanoid } from "nanoid";
 import { LiveObject } from "@liveblocks/client";
@@ -40,9 +41,9 @@ export const Canvas = ({ boardId }: CanvasProps) => {
 
 	const [camera, setCamera] = useState<Camera>({ x: 0, y: 0 });
 	const [lastUsedColor, setLastUsedColor] = useState<Color>({
-		r: 255,
-		g: 255,
-		b: 255,
+		r: 0,
+		g: 0,
+		b: 0,
 	});
 
 	const history = useHistory();
@@ -129,6 +130,49 @@ export const Canvas = ({ boardId }: CanvasProps) => {
 		[camera, canvasState, history, insertLayer]
 	);
 
+	const selections = useOthersMapped((other) => other.presence.selection);
+
+	const onLayerPointerDown = useMutation(
+		({ self, setMyPresence }, e: React.PointerEvent, layerId: string) => {
+			if (
+				canvasState.mode === CanvasMode.Pencil ||
+				canvasState.mode === CanvasMode.Inserting
+			) {
+				return;
+			}
+
+			history.pause();
+			e.stopPropagation();
+
+			const point = pointerEventToCanvasPoint(e, camera);
+
+			if (!self.presence.selection.includes(layerId)) {
+				setMyPresence({ selection: [layerId] }, { addToHistory: true });
+			}
+
+			setCanvasState({
+				mode: CanvasMode.Translating,
+				current: point,
+			});
+		},
+		[setCanvasState, camera, history, canvasState.mode]
+	);
+
+	const layerIdsToColorSelection = useMemo(() => {
+		const layerIdsToColorSelection: Record<string, string> = {};
+
+		for (const user of selections) {
+			const [connectionId, selection] = user;
+
+			for (const layerId of selection) {
+				layerIdsToColorSelection[layerId] =
+					connectionIdToColor(connectionId);
+			}
+		}
+
+		return layerIdsToColorSelection;
+	}, [selections]);
+
 	return (
 		<main className="w-full h-full relative bg-neutral-100 touch-none">
 			<Info boardId={boardId} />
@@ -157,8 +201,8 @@ export const Canvas = ({ boardId }: CanvasProps) => {
 						<LayerPreview
 							key={layerId}
 							id={layerId}
-							onLayerPointerDown={() => {}}
-							selectionColor="#000"
+							onLayerPointerDown={onLayerPointerDown}
+							selectionColor={layerIdsToColorSelection[layerId]}
 						/>
 					))}
 					<CursorsPresence />
--- miro/middleware.ts
+++ miro/middleware.ts
@@ -1,12 +1,25 @@
 import { withClerkMiddleware } from "@clerk/nextjs/server";
 
-export default withClerkMiddleware({
-	publicRoutes: ["/contact", "/api(.)*"],
-});
+export default withClerkMiddleware();
 
 export const config = {
 	matcher: [
+		// Skip Next.js internals and all static files, unless found in search params
 		"/((?!_next|[^?]*\\.(?:html?|css|js(?!on)|jpe?g|webp|png|gif|svg|ttf|woff2?|ico|csv|docx?|xlsx?|zip|webmanifest)).*)",
+		// Always run for API routes
 		"/(api|trpc)(.*)",
 	],
 };
+
+// import { withClerkMiddleware } from "@clerk/nextjs/server";
+
+// export default withClerkMiddleware({
+// 	publicRoutes: ["/contact", "/api(.)*"],
+// });
+
+// export const config = {
+// 	matcher: [
+// 		"/((?!_next|[^?]*\\.(?:html?|css|js(?!on)|jpe?g|webp|png|gif|svg|ttf|woff2?|ico|csv|docx?|xlsx?|zip|webmanifest)).*)",
+// 		"/(api|trpc)(.*)",
+// 	],
+// };
